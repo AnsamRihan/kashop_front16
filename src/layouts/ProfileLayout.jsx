8a6724d190803bd157dc0fc10db2d5c192e44d3d
AnsamRihan <ansam.rihan2014@gmail.com>
2026-08-25 16:42:06 +0300

    edit profile layout

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -1,3 +1,8 @@
+import CircularProgress from '@/components/CircularProgress/CircularProgress';
+import ErrorFetchingData from '@/components/ErrorFetchingData.jsx/ErrorFetchingData';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import useProfile from '@/hooks/useProfile';
+import useUserStore from '@/store/useUserStore';
 import { FingerprintPattern, Folders } from 'lucide-react';
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -5,21 +10,56 @@ import { Link, Outlet } from 'react-router-dom';
 
 export default function ProfileLayout() {
     const { t } = useTranslation("profileLayout");
+    const { isLoading, isError, error } = useProfile();
+    const user = useUserStore((set) => set.user);
+
     return (
         <>
             <section className='p-10'>
                 <div className="container">
-                    <div className='w-full grid grid-cols-1 md:grid-cols-[1.75fr_4fr] gap-6'>
-                        <div className='stack gap-1 w-full rounded-lg bg-secondary-background p-4 h-fit'>
+                    <div className='w-full grid grid-cols-1 md:grid-cols-[1.75fr_4fr] lg:grid-cols-[1.5fr_4fr] xl:grid-cols-[1.35fr_4fr] 2xl:grid-cols-[1.25fr_4fr] gap-6'>
+                        <div className='stack gap-1 w-full rounded-lg bg-secondary-background p-3 h-fit'>
+
+                            <div className="flex h-auto w-full justify-start gap-3 p-2 text-foreground mb-2">
+
+                                {isLoading && (
+                                    <CircularProgress />
+                                )}
+
+                                {isError && (
+                                    <ErrorFetchingData error={error} />
+                                )}
+
+                                {!isLoading && !isError && user && (
+                                    <>
+                                        <Avatar className="h-10 w-10 circle">
+                                            <AvatarFallback className="circle bg-primary text-primary-foreground">
+                                                {user.fullName?.slice(0, 2).toUpperCase()}
+                                            </AvatarFallback>
+                                        </Avatar>
+
+                                        <div className="grid min-w-0 flex-1 text-start text-sm lg:text-base leading-tight">
+                                            <span className="truncate font-semibold text-primary">
+                                                {user.fullName}
+                                            </span>
+
+                                            <span className="truncate text-xs text-muted-foreground">
+                                                {user.email}
+                                            </span>
+                                        </div>
+                                    </>
+                                )}
+                            </div>
+
                             <Link to="" className='w-full px-4 py-2 rounded-lg hover:bg-primary hover:text-primary-foreground
-                            row transition-all duration-150 ease-in-out'>
-                                <FingerprintPattern className='size-5' />
+                            row transition-all duration-150 ease-in-out text-sm lg:text-base'>
+                                <FingerprintPattern className='size-4 lg:size-5' />
                                 {t("profile")}
                             </Link>
 
                             <Link to="orders" className='w-full px-4 py-2 rounded-lg hover:bg-primary hover:text-primary-foreground
-                            row transition-all duration-150 ease-in-out'>
-                                <Folders className='size-5' />
+                            row transition-all duration-150 ease-in-out text-sm lg:text-base'>
+                                <Folders className='size-4 lg:size-5' />
                                 {t("myOrders")}
                             </Link>
                         </div>
