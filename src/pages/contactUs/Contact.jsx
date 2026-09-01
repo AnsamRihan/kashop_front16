d16ae31580428d7faaa83c8f86ee1414c7d6d43a
AnsamRihan <ansam.rihan2014@gmail.com>
2026-09-01 20:39:26 +0300

    create how can me help section in contact page

--- src/pages/contactUs/Contact.jsx
+++ src/pages/contactUs/Contact.jsx
@@ -1,6 +1,23 @@
 import { BubbleBackground } from '@/components/animate-ui/backgrounds/bubble';
+import { Button } from '@/components/ui/button';
+import { Mail, MessageSquareText, Phone } from 'lucide-react';
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+
+export const contactOptions = [
+  {
+    key: "chat",
+    icon: MessageSquareText,
+  },
+  {
+    key: "email",
+    icon: Mail,
+  },
+  {
+    key: "call",
+    icon: Phone,
+  },
+];
 
 export default function Contact() {
   const { t } = useTranslation("contact");
@@ -8,7 +25,7 @@ export default function Contact() {
   return (
     <>
       {/*Header */}
-      <section className="pb-8 text-center">
+      <section className="text-center">
         <BubbleBackground interactive>
           <div className="container">
             <div className="relative z-10 w-full center">
@@ -26,7 +43,50 @@ export default function Contact() {
         </BubbleBackground>
       </section>
 
-      
+      {/*how can we help section */}
+      <section className='py-16 text-center'>
+        <div className="container">
+          <div className="stack justify-center gap-8 md:gap-12">
+            <h2 className='text-heading-foreground font-semibold tracking-[-0.32px] text-2xl xxs:text-3xl xs:text-4xl'>
+              {t("howCanWeHelp")}
+            </h2>
+
+            <div className='w-full flex flex-wrap justify-center gap-6'>
+              {contactOptions.map((option) => {
+                const Icon = option.icon;
+
+                return (
+                  <div key={option.key} className='w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]'>
+                    <div className='w-full h-full stack justify-between gap-6 p-8'>
+                      <div className='w-full stack gap-6'>
+                        {/*icon */}
+                        <div className='w-16 h-16 circle bg-primary/10 gap-4.5 center'>
+                          <Icon className='text-primary' />
+                        </div>
+
+                        {/*text */}
+                        <div className='stack gap-3'>
+                          <h3 className='text-heading-foreground font-semibold text-xl'>
+                            {t(`contactOptions.${option.key}.title`)}
+                          </h3>
+
+                          <p>
+                            {t(`contactOptions.${option.key}.description`)}
+                          </p>
+                        </div>
+                      </div>
+
+                      <Button variant='gradiant' className='w-full'>
+                        {t(`contactOptions.${option.key}.button`)}
+                      </Button>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
